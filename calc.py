47b0cd33b81ba23d1e387a305b4bc867d77c7ed3
Hamid Adesokan <aadehamid@gmail.com>
2020-10-21 00:40:44 -0500

    Initial commit

---
+++ calc.py
@@ -0,0 +1,32 @@
+# %%
+def add(x, y):
+    """Add Function"""
+    if (type(x) or type(y)) not in [int, float]:
+        raise TypeError("The inputs must be of type int or floats.")
+    return x + y
+# %%
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    if (type(x) or type(y)) not in [int, float]:
+        raise TypeError("The inputs must be of type int or floats.")
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    if (type(x) or type(y)) not in [int, float]:
+        raise TypeError("The inputs must be of type int or floats.")
+    return x * y
+
+
+def divide(x, y):
+    """Divide Function"""
+    if (type(x) or type(y)) not in [int, float]:
+        raise TypeError("The inputs must be of type int or floats.")
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return x / y
+
+# %%
